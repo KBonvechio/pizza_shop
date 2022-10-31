5dbbdc8c9b84c9f1a9026997ac779a0ba816a45c
KBonvechio <115104521+KBonvechio@users.noreply.github.com>
2022-10-31 17:19:16 -0400

    Used dplyr functions and piping to merge all 16 pizza datasets together.

--- sales/sales_script.R
+++ sales/sales_script.R
@@ -15,13 +15,38 @@ for (salesdata in all_sales){
   i <- i + 1
 }
 
+all_sales
+
 ############## JOIN ###############
 # Use a tidyverse join to join all the data together into one file
 # called sales_data, then run the rest of the code
 
+# Answer
+# I was able to use the full_join() function to combine 12 of the 16 datasets.
+# However, there were some values in four of the datasets that had matching rows
+# with the dataset I was trying to join them with (i.e., each set of datasets had
+# some rows with identical values for each variable/column), so I had to 
+# use the bind_rows() function (also in dplyr) to add the final four datasets. I 
+# also used the select() function to remove the extra "date" column that was 
+# carried over from the merge with dataset_15. This is the only way I could get
+# the sales_data dataset to contain all 896 rows of data.
 
+sales_data <- dataset_1 %>%
+  full_join(dataset_2, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_4, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_6, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_7, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_8, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_9, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_10, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_13, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_14, by = c("day", "month", "year", "pizza", "number")) %>%
+  full_join(dataset_15, by = c("day", "month", "year", "pizza", "number")) %>%
+  select("day", "month", "year", "pizza", "number") %>%
+  full_join(dataset_16, by = c("day", "month", "year", "pizza", "number")) %>%
+  bind_rows(dataset_3, dataset_5, dataset_11, dataset_12)
 
-########################################
+  ########################################
 
 ##### 3. Create summaries #####
 sales_summary <- sales_data %>%
